350249515a8dc94fd8ecbe06291f25f52117f7ec
slendchat <slendchat@gmail.com>
2025-02-20 08:36:41 +0000

    removed old configuration

--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Script to build and run the Docker containers
-
-# echo "Entering the directory with app"
-# cd app
-
-# echo "Compiling java app"
-# sudo mvn clean package
-
-# echo "Going back"
-# cd .. 
-
 echo "🚀 Stopping any running containers..."
 docker-compose down
 
