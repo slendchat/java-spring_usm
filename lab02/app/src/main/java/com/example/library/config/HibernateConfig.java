e0a613978d1a083107e7288c5b5799464db525d9
slendchat <slendchat@gmail.com>
2025-03-11 14:36:10 +0200

    8765765

--- lab02/app/src/main/java/com/example/library/config/HibernateConfig.java
+++ lab02/app/src/main/java/com/example/library/config/HibernateConfig.java
@@ -4,9 +4,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -16,10 +16,10 @@ public class HibernateConfig {
     @Bean
     public SessionFactory sessionFactory(DataSource dataSource) {
         Properties props = new Properties();
-        props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         props.put("hibernate.hbm2ddl.auto", "update");
-        props.put("hibernate.show_sql", "true");
-        props.put("hibernate.current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
+        props.put("hibernate.show_sql", true);
+        props.put("hibernate.current_session_context_class", 
+                  "org.springframework.orm.hibernate5.SpringSessionContext");
 
         Configuration cfg = new Configuration();
         cfg.setProperties(props);
@@ -37,7 +37,6 @@ public class HibernateConfig {
     }
 
     @Bean
-    @Primary
     public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
         return new HibernateTransactionManager(sessionFactory);
     }
