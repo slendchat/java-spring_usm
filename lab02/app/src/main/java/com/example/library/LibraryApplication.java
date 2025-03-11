046d33cfb627f5ba113e198104fac7c6a300bd32
slendchat <slendchat@gmail.com>
2025-03-11 15:04:02 +0200

    4235432543

--- lab02/app/src/main/java/com/example/library/LibraryApplication.java
+++ lab02/app/src/main/java/com/example/library/LibraryApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude = {
-    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
 })
 public class LibraryApplication {
     public static void main(String[] args) {
