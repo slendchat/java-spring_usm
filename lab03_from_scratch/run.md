f8720c1307952a8da4fedabe3bd4969d5d5306e7
slendchat <slendchat@gmail.com>
2025-04-25 14:09:31 +0000

    h2 prikolna java zalupa

---
+++ lab03_from_scratch/run.md
@@ -0,0 +1,3 @@
+mvn clean package
+mvn spring-boot:run
+java -jar target/sport-0.0.1-SNAPSHOT.jar
--- lab03_from_scratch/src/main/java/com/lab03/sport/SportApplication.java
+++ lab03_from_scratch/src/main/java/com/lab03/sport/SportApplication.java
@@ -3,23 +3,29 @@ package com.lab03.sport;
 import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class SportApplication {
 
-	public static void main(String[] args) {
-		try {
-			Server.createTcpServer(
-				"-tcp",
-				"-tcpAllowOthers",
-				"-tcpPort", "9092"
-			).start();
-		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-			return;
-		}
-		SpringApplication.run(SportApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        try {
+            // HTTP-консоль на 8082, доступ с любых хостов
+            Server.createWebServer(
+                "-web",
+                "-webAllowOthers", 
+                "-webPort", "8082"
+            ).start();
+            
+            // TCP-сервер на 9092, доступ с любых хостов
+            Server.createTcpServer(
+                "-tcp",
+                "-tcpAllowOthers",
+                "-tcpPort", "9092"
+            ).start();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        SpringApplication.run(SportApplication.class, args);
+    }
 }
